1ecdfa8617fe3a8c608a489c228c88130cb4c9d9
Yusuf Dogan <yusuf.dogan@ug.bilkent.edu.tr>
2024-05-05 13:06:46 +0300

    coma fix

--- webapp/src/main/java/com/kolaykira/webapp/user/UserController.java
+++ webapp/src/main/java/com/kolaykira/webapp/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.google.cloud.Timestamp;;
+import com.google.cloud.Timestamp;
 import java.util.concurrent.ExecutionException;
 
 @RestController
